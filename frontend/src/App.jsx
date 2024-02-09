5d8a0e6b1b98ece8ad67b445a710ceebdc01dbe2
Ridwan-Noor <ridwan.noor.tasin@g.bracu.ac.bd>
2024-02-09 23:20:37 +0600

    added environment files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.css" // updated
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from "./components/login"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>} > </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
