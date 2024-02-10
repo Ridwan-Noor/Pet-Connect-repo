534b063feeda31763226cca15b6b25df6e043bd8
Ridwan-Noor <ridwan.noor.tasin@g.bracu.ac.bd>
2024-02-10 21:42:53 +0600

    Revert "Delete frontend directory"
    This reverts commit 84422ce912767a10d260784a4bd1285b16c9aec3.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/components/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      
+    },
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+    }
+  },
+  plugins: [],
+  
+}
