1543eed625e6d4a055d7926cab10213c8ee915ba
NafisaMehreen <100036933+NafisaMehreen@users.noreply.github.com>
2024-02-26 01:23:15 +0600

    update

--- frontend/src/components/Signup/signup.jsx
+++ frontend/src/components/Signup/signup.jsx
@@ -9,7 +9,13 @@ const signup = () => {
         <div className='flex justify-center items-center'>
             <div className='w-full md:w-3/4 lg:w-1/2 xl:w-2/5'>
                 <div className='flex flex-col md:flex-row'>
-                    
+                    <div className='md: w-1/2 bg-white rounded-tl-lg md:rounded-bl-lg p-8 flex flex-col items-center justify-center'>
+                      <h1 className='text-3x1 font-bold mb-4'>Welcome to PetConnect</h1>
+                      <img src={SignupImg} alt='Signup' className='w-64 mb-8'/>
+                      <Link to='/login'>
+                        <button type='button' className='bg-white rounded-lg w-96 p-3 font-semibold text-medium'></button>
+                      </Link>
+                    </div>
                 </div>
             </div>
         </div>
