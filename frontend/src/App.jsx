56b505a093ce3e966885765d43bfd0d69839a110
NafisaMehreen <100036933+NafisaMehreen@users.noreply.github.com>
2024-03-15 02:37:50 +0600

    update profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.css" // updated
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./components/Login/login"
 import Signup from "./components/Signup/signup"
+import Profile from "./components/Profile/profile"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login/>} > </Route>
         <Route path='/signup' element={<Signup/>} ></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
       </Routes>
     </BrowserRouter> 
   )
