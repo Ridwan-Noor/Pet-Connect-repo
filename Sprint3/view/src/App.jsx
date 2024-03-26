efe78fa1ecf23aea274b0de6372b04062a7bcabd
NafisaMehreen <100036933+NafisaMehreen@users.noreply.github.com>
2024-03-26 12:18:39 +0600

    postevent

--- Sprint3/view/src/App.jsx
+++ Sprint3/view/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./components/Profile/profile"
 import Events from "./components/Events/events"
 import Home from "./components/home.jsx"
 import Resources from "./components/resources.jsx"
+import PostEvents from "./components/PostEvent/postevent"
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/events' element={<Events/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
         <Route path='/resources' element={<Resources/>}></Route>
+        <Route path='/postevent' element={<PostEvents/>}></Route>
       
       </Routes>
     </BrowserRouter> 
