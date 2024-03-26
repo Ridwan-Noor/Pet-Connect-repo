d7f2f4fa542817d0779da833f04b95e0109f571f
Ridwan-Noor <ridwan.noor.tasin@g.bracu.ac.bd>
2024-03-26 22:35:18 +0600

    navbar

--- Sprint3/view/src/App.jsx
+++ Sprint3/view/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./components/Profile/profile"
 import Events from "./components/Events/events"
 import Home from "./components/home.jsx"
 import Resources from "./components/resources.jsx"
+import PetShop from "./components/petShop.jsx"
 
 export const UserContext = createContext(null)
 
@@ -23,6 +24,7 @@ function App() {
           <Route path='/events' element={<Events/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path='/resources' element={<Resources/>}></Route>
+          <Route path='/petShop' element={<PetShop/>}></Route>
         
         </Routes>        
       </UserContext.Provider>
