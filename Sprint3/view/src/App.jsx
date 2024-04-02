3f6083371a4e5d215e9db727a2e1a962df345b6d
Aritra Ghose <149812136+aaritraa@users.noreply.github.com>
2024-04-03 00:26:33 +0600

    changes made

--- Sprint3/view/src/App.jsx
+++ Sprint3/view/src/App.jsx
@@ -4,30 +4,38 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./components/Login/login"
 import Signup from "./components/Signup/signup"
 import Profile from "./components/Profile/profile"
+import Services from "./components/Services/services.jsx"
 import Events from "./components/Events/events"
-import Home from "./components/Homepage/home.jsx"
-import Resources from "./components/Resources/resources.jsx"
-import PostEvents from "./components/PostEvent/postevent"
-import PetShop from "./components/Petshop/petShop.jsx"
+import Home from "./components/home.jsx"
+import Resources from "./components/resources.jsx"
+import VetProfile from "./components/Profile/vetProfile.jsx"
+import EditVetProfile from "./components/EditProfile/editprofilevet.jsx"
+import Messages from "./components/Inbox/messages.jsx"
+// import Conversation from "./components/Inbox/conversation.jsx"
+// <Route path='/conversation' element={<Conversation/>}></Route>
 
-export const UserContext = createContext("none")
+export const UserContext = createContext(null)
 
 function App() {
   const [u, setU] = useState("none")
 
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{u, setU}}>    
+      <UserContext.Provider value={{u, setU}}>
         <Routes>
           <Route path='/login' element={<Login/>} > </Route>
           <Route path='/signup' element={<Signup/>} ></Route>
           <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/services' element={<Services/>}></Route>
           <Route path='/events' element={<Events/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path='/resources' element={<Resources/>}></Route>
-          <Route path='/postevent' element={<PostEvents/>}></Route>
-          <Route path='/petShop' element={<PetShop/>}></Route>
+          <Route path='/vetProfile' element={<VetProfile/>}></Route>
+          <Route path='/editprofilevet' element={<EditVetProfile/>}></Route>
+          <Route path='/messages' element={<Messages/>}></Route>
           
+          
+        
         </Routes>        
       </UserContext.Provider>
 
